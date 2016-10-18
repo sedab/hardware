a90ccf92829a65f55df5b2b6b76170cc80bf9682
borisnimcevic <borisnimcevic@gmail.com>
2016-10-18 10:56:29 -0700

    Accelerometer code ready for testing testig the sensor

--- Acceleration_Fit_AI/Acceleration_Fit_AI.ino
+++ Acceleration_Fit_AI/Acceleration_Fit_AI.ino
@@ -1,69 +1,58 @@
+/*
+ * This code is made for testing accelerometer
+ */
+
 #include <math.h>
 //#include <Average.h>
 
+//*** Accelerometer Pins***
 const int GND = A5;
 const int Zaxis = A2;
 const int Yaxis = A3;
 const int Xaxis = A4;
 const int VCC = A1;
 
-int totalAcceleration;
+
 int xAcc, yAcc, zAcc;
-int rest;
+
 
 void setup() {
+  //start serial
   Serial.begin(9600);
+  while(!Serial){
+    //wait for the serial connection
+  }
   Serial.println("Hi!");
 
+  //Configure the accelerometer pins
   pinMode(GND,OUTPUT);
   pinMode(VCC,OUTPUT);
   digitalWrite(GND,LOW);
   digitalWrite(VCC,HIGH);
-
-  
   pinMode(Xaxis, INPUT);
   pinMode(Yaxis, INPUT);
   pinMode(Zaxis, INPUT);
-
-
-//  rest = sqrt(square(xAcc) + square(yAcc) + square(zAcc));
-  /*
-    for (int i = 0; i < 99; i++) {
-    xAcc = analogRead(Xaxis);
-    yAcc = analogRead(Yaxis);
-    zAcc = analogRead(Zaxis);
-    totalAcceleration = sqrt(square(xAcc) + square(yAcc) + square(zAcc));
-    rest[i] = totalAcceleration;
-    delay(10);
-    }
-    for(int k=0;k<99;k++){
-    total=total+rest[k];
-    Serial.println(total);
-    }
-    total=(total/100);
-    Serial.println("This is the average value");
-    Serial.println(total);
-  */
 }
 
 void loop() {
-  //Serial.println(rest);
 
+  //read the values from the accelerometer
   xAcc = analogRead(Xaxis);
   yAcc = analogRead(Yaxis);
   zAcc = analogRead(Zaxis);
 
+  //display all the values
   delay(10);
+  Serial.print("X: ");
   Serial.print(xAcc);
   Serial.print("\t");
+  Serial.print("Y: ");
   Serial.print(yAcc);
   Serial.print("\t");
+  Serial.print("Z: ");
   Serial.print(zAcc);
   Serial.print("\t");
   Serial.println(" ");
 
-
-  delay(700);
-
-
+  delay(500);
 }
